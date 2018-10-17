c74009786d18585816817d62d211b9e40709e4fa
Gabor Vidovics <gabor.vidovics@dvcon.hu>
2018-10-18 01:31:25 +0200

    First commit

---
+++ git_test_project/WebContent/git_test_project/Object.controller.js
@@ -0,0 +1,38 @@
+sap.ui.controller("git_test_project.Object", {
+
+/**
+* Called when a controller is instantiated and its View controls (if available) are already created.
+* Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
+* @memberOf git_test_project.Object
+*/
+//	onInit: function() {
+//
+//	},
+
+/**
+* Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
+* (NOT before the first rendering! onInit() is used for that one!).
+* @memberOf git_test_project.Object
+*/
+//	onBeforeRendering: function() {
+//
+//	},
+
+/**
+* Called when the View has been rendered (so its HTML is part of the document). Post-rendering manipulations of the HTML could be done here.
+* This hook is the same one that SAPUI5 controls get after being rendered.
+* @memberOf git_test_project.Object
+*/
+//	onAfterRendering: function() {
+//
+//	},
+
+/**
+* Called when the Controller is destroyed. Use this one to free resources and finalize activities.
+* @memberOf git_test_project.Object
+*/
+//	onExit: function() {
+//
+//	}
+
+});
